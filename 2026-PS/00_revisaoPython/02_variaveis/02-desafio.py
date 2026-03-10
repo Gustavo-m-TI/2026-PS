d2c247dd1c06a449ade34f04f2668cf8b8f2e795
Gustavo-m-TI <20251ctb0100029@estudantes.ifpr.edu.br>
2026-03-10 13:32:47 +0000

    coisado

---
+++ 2026-PS/00_revisaoPython/02_variaveis/02-desafio.py
@@ -0,0 +1,15 @@
+# ===================================================
+# SISTEMA DE APROVAÇÃO DE ALUNOS
+# ===================================================
+# Disciplina : Programação de Sistemas (PS)
+# Aula       : 04 – Revisão: Variáveis, Tipos e Controle de Fluxo
+# Autor      : [Gustavo Maciel da Silva]
+# Data       : [10.03.2026]
+# Repositório: https://github.com/[Gustavo-m-TI]/2026-PS
+# ===================================================
+#
+# DESCRIÇÃO:
+# Este programa funciona como um estoque de produtos,
+# ele contém três produtos pré cadastrados e xibe a quantidade
+# 
+# ===================================================
